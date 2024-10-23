46d7e0f03bcb8d26d2b3933bf7704a56b7681af7
Alper <hansd4243@gmail.com>
2024-10-23 15:58:27 +0200

    1.COMMIT HAUSAUFGABE TOM 231024

--- HausaufgabeReactAws-24-06/RRREACT-aws-24-06/src/App.js
+++ HausaufgabeReactAws-24-06/RRREACT-aws-24-06/src/App.js
@@ -2,13 +2,17 @@ import logo from "./logo.svg";
 import "./App.css";
 import Navbar from "./components/layout/navbar/Navbar";
 import Footbar from "./components/layout/footbar/Footbar";
+import Buttontest from "./components/layout/buttontest/Buttontest";
 
 function App() {
   return (
     <div className="main-container">
       <Navbar />
+      <Buttontest />
       <div className="content">Hello from Homepage</div>
+      
        <Footbar />
+      
     </div>
   );
 }
---
+++ HausaufgabeReactAws-24-06/RRREACT-aws-24-06/src/components/layout/buttontest/Buttontest.jsx
@@ -0,0 +1,15 @@
+/*FUNKTION/ button Buttontest - eine funktion für den Button mit in Butttonteest.jsx .buttonteest // styles von Buttonteest className=*/
+import styles from './Buttontest.module.css'; // CSS importieren
+
+function Buttontest() {
+    return (
+        <button className={styles.buttontest}>
+            Klick mich
+        </button>
+    );
+}
+
+export default Buttontest;
+
+
+/*https://github.com/tomtechstarter/react-aws-24-06/blob/main/src/components/common/standard-btn/StandardBtn.jsx*/
